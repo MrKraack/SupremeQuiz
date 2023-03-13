3c3fd3df4d21cfe77e92a6bb8513fe00c037ed94
caldecreativo <93321331+caldecreativo@users.noreply.github.com>
2023-03-13 13:07:04 +0100

    quizrouters changed

---
+++ routes/quiz/loadQuiz.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const quizSchema = mongoose.model("quiz", quizSchema)
+
+module.exports = async (res) => {
+    const quizModel = await quizSchema.find({});
+   
+}
--- routes/router.js
+++ routes/router.js
@@ -2,9 +2,20 @@ const express = require('express')
 
 const userCreateRoute = require('./user/createUser')
 
+const quizLoadRoute = require('./quiz/loadQuiz')
+
+const quizCreateRoute = require('./quiz/createQuiz')
+
+const quizDeleteRoute = require('./quiz/deleteQuiz')
+
 const router = express.Router()
 
 //User routes
 router.post('/register', userCreateRoute)
+
+//quiz routes
+router.get('/loadQuiz', quizLoadRoute)
+router.post('/createQuiz', quizCreateRoute)
+// router.post('/deleteQuiz', quizDeleteRoute)
 
 module.exports = router
