1c26a5f5d55f1d1b5703295e96ce59bf344fc983
Artur Pilarek <artur.pilarek@gmail.com>
2023-03-14 08:54:05 +0100

    update user route created, implemented and tested

--- routes/router.js
+++ routes/router.js
@@ -3,6 +3,7 @@ const express = require('express')
 const userCreateRoute = require('./user/createUser')
 const userLoginRoute = require('./user/loginUser')
 const userDeleteRoute = require('./user/deleteUser')
+const userUpdateRoute = require('./user/updateUser')
 
 const quizLoadRoute = require('./quiz/loadQuiz')
 
@@ -15,6 +16,7 @@ const router = express.Router()
 //User routes
 router.post('/register', userCreateRoute)
 router.post('/loginUser', userLoginRoute)
+router.put('/updateUser/:id', userUpdateRoute)
 router.delete('/deleteUser/:id', userDeleteRoute)
 
 
