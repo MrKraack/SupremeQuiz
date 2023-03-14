5b78e9cd4dfdc95fbb5cc685d3367ab50d01cbf5
Artur Pilarek <artur.pilarek@gmail.com>
2023-03-14 11:55:02 +0100

    Implemented passport.js as the session authentication

--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const passport = require('passport');
+const connectEnsureLogin = require('connect-ensure-login')
 
 const userCreateRoute = require('./user/createUser')
 const userLoginRoute = require('./user/loginUser')
@@ -13,6 +14,39 @@ const quizCreateRoute = require('./quiz/createQuiz')
 const quizDeleteRoute = require('./quiz/deleteQuiz')
 
 const router = express.Router()
+
+
+
+//Auth routes
+router.post(
+    '/login',
+    passport.authenticate('local', {
+        failureRedirect: '/login',
+        successRedirect: '/',
+    })
+);
+
+router.get('/login', function(req, res, next) {
+    res.render('login');
+});
+
+router.get('/signup', function(req, res, next) {
+    res.render('signup');
+});
+
+//Website protected routes
+router.get('/', connectEnsureLogin.ensureLoggedIn(), function(req, res, next) {
+    res.render('index');
+});
+router.get('/user', connectEnsureLogin.ensureLoggedIn(), function(req, res, next) {
+    res.render('user');
+});
+router.get('/addquiz', connectEnsureLogin.ensureLoggedIn(), function(req, res, next) {
+    res.render('addquiz');
+});
+router.get('/stats', connectEnsureLogin.ensureLoggedIn(), function(req, res, next) {
+    res.render('stats');
+});
 
 //User routes
 router.post('/register', userCreateRoute)
